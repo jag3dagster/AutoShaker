544ad8c85821a5f34c3beb7a77648e44ec4bb250
Jag3Dagster <jag3dagster@gmail.com>
2021-01-19 22:47:59 -0800

    Removing GMCM dll reference and swapping to the expected expected Interface smapi system

--- src/AutoShaker/ModEntry.cs
+++ src/AutoShaker/ModEntry.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
-using SpaceShared.APIs;
 using StardewModdingAPI;
 using StardewModdingAPI.Events;
 using StardewValley;
@@ -21,8 +20,8 @@ namespace AutoShaker
 
 		private readonly List<Bush> _shakenBushes = new List<Bush>();
 
-		private int _treesShaken = 0;
-		private int _fruitTressShaken = 0;
+		private int _treesShaken;
+		private int _fruitTressShaken;
 
 		/// <summary>
 		/// The mod entry point, called after the mod is first loaded.
@@ -32,10 +31,10 @@ namespace AutoShaker
 		{
 			_config = helper.ReadConfig<ModConfig>();
 
-			helper.Events.GameLoop.UpdateTicked += this.OnUpdateTicked;
-			helper.Events.GameLoop.DayEnding += this.OnDayEnding;
-			helper.Events.Input.ButtonPressed += this.OnButtonPressed;
-			helper.Events.GameLoop.GameLaunched += this.OnGameLaunched;
+			helper.Events.GameLoop.UpdateTicked += OnUpdateTicked;
+			helper.Events.GameLoop.DayEnding += OnDayEnding;
+			helper.Events.Input.ButtonPressed += OnButtonPressed;
+			helper.Events.GameLoop.GameLaunched += (_,__) => _config.RegisterModConfigMenu(helper, ModManifest);
 		}
 
 		private void OnUpdateTicked(object sender, UpdateTickedEventArgs e)
@@ -174,30 +173,13 @@ namespace AutoShaker
 
 			if (_config.UsePlayerMagnetism)
 			{
-				pickUpDistance = (int)Math.Floor((double)playerMagnetism / (double)SingleTileDistance);
+				pickUpDistance = (int)Math.Floor(playerMagnetism / (double)SingleTileDistance);
 			}
 
 			var inRange = Math.Abs(bushLocation.X - playerLocation.X) <= pickUpDistance &&
 							Math.Abs(bushLocation.Y - playerLocation.Y) <= pickUpDistance;
 
 			return inRange;
-		}
-
-		private void OnGameLaunched(object sender, GameLaunchedEventArgs e)
-		{
-			var gmcmApi = this.Helper.ModRegistry.GetApi<GenericModConfigMenuAPI>("spacechase0.GenericModConfigMenu");
-
-			if (gmcmApi != null)
-			{
-				gmcmApi.RegisterModConfig(ModManifest, () => _config = new ModConfig(), () => Helper.WriteConfig(_config));
-
-				gmcmApi.RegisterSimpleOption(ModManifest, "Shaker Is Active", "Whether or not the AutoShaker mod is active.", () => _config.IsShakerActive, (val) => _config.IsShakerActive = val);
-				gmcmApi.RegisterSimpleOption(ModManifest, "Toggle Shaker Keybind", "Keybinding to toggle the AutoShaker on and off.", () => _config.ToggleShaker, (val) => _config.ToggleShaker = val);
-				gmcmApi.RegisterSimpleOption(ModManifest, "Shake Trees?", "Whether or not the AutoShaker will shake trees that you walk by.", () => _config.ShakeTrees, (val) => _config.ShakeTrees = val);
-				gmcmApi.RegisterSimpleOption(ModManifest, "Shake Bushes?", "Whether or not the AutoShaker will shake bushes that you walk by.", () => _config.ShakeBushes, (val) => _config.ShakeBushes = val);
-				gmcmApi.RegisterSimpleOption(ModManifest, "Use Player Magnetism Distance?", "Whether or not the AutoShaker will shake bushes at the same distance players can pick up items. Note: Overrides 'Shake Distance'", () => _config.UsePlayerMagnetism, (val) => _config.UsePlayerMagnetism = val);
-				gmcmApi.RegisterSimpleOption(ModManifest, "Shake Distance", "Distance to shake bushes when not using 'Player Magnetism.'", () => _config.ShakeDistance, (val) => _config.ShakeDistance = val);
-			}
 		}
 	}
 }
